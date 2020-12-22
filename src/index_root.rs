45d4e97b83b0cbd14b765bff0e34f2e522eb3a9e
Colin Finck <colin@reactos.org>
2020-12-22 17:18:31 +0100

    Unify the code for IndexRootIter and IndexRootIterMut
    There is no reason to keep up two fundamentally different implementations.

--- src/index_root.rs
+++ src/index_root.rs
@@ -6,10 +6,10 @@
 //!
 
 use crate::error::{NtHiveError, Result};
-use crate::fast_leaf::{FastLeafElement, FastLeafIter};
-use crate::hash_leaf::{HashLeafElement, HashLeafIter};
+use crate::fast_leaf::FastLeafElement;
+use crate::hash_leaf::HashLeafElement;
 use crate::hive::Hive;
-use crate::index_leaf::{IndexLeafElement, IndexLeafIter};
+use crate::index_leaf::IndexLeafElement;
 use crate::key_node::KeyNode;
 use crate::subkeys_list::SubkeysList;
 use ::byteorder::LittleEndian;
@@ -61,7 +61,7 @@ impl IndexRootElement {
 pub struct IndexRootIter<'a, B: ByteSlice> {
     hive: &'a Hive<B>,
     elements_range: Range<usize>,
-    inner_iter: Option<IndexRootInnerIter<'a, B>>,
+    inner_info: Option<IndexRootIterInnerInfo>,
 }
 
 impl<'a, B> IndexRootIter<'a, B>
@@ -80,7 +80,7 @@ where
         Ok(Self {
             hive,
             elements_range,
-            inner_iter: None,
+            inner_info: None,
         })
     }
 }
@@ -93,11 +93,12 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(iter) = &mut self.inner_iter {
-                // Retrieve the next element from the inner iterator.
-                let item = iter.next();
-                if item.is_some() {
-                    return item;
+            if let Some(inner_info) = self.inner_info.as_mut() {
+                if let Some(key_node_offset) = inner_info.next_key_node_offset(&self.hive) {
+                    let cell_range =
+                        iter_try!(self.hive.cell_range_from_data_offset(key_node_offset));
+                    let key_node = iter_try!(KeyNode::new(self.hive, cell_range));
+                    return Some(Ok(key_node));
                 }
             }
 
@@ -114,67 +115,69 @@ where
                 iter_try!(SubkeysList::new_without_index_root(self.hive, cell_range));
 
             let header = subkeys_list.header();
+            let data_range = subkeys_list.data_range.clone();
+            let signature = header.signature;
             let count = header.count.get();
             let count_field_offset = self.hive.offset_of_field(&header.count);
-            self.inner_iter = match &header.signature {
+
+            let elements_range = match &signature {
                 b"lf" => {
                     // Fast Leaf
-                    let iter = iter_try!(FastLeafIter::new(
-                        &self.hive,
+                    iter_try!(FastLeafElement::elements_range(
                         count,
                         count_field_offset,
-                        subkeys_list.data_range
-                    ));
-                    Some(IndexRootInnerIter::FastLeaf(iter))
+                        data_range
+                    ))
                 }
                 b"lh" => {
                     // Hash Leaf
-                    let iter = iter_try!(HashLeafIter::new(
-                        &self.hive,
+                    iter_try!(HashLeafElement::elements_range(
                         count,
                         count_field_offset,
-                        subkeys_list.data_range
-                    ));
-                    Some(IndexRootInnerIter::HashLeaf(iter))
+                        data_range
+                    ))
                 }
                 b"li" => {
                     // Index Leaf
-                    let iter = iter_try!(IndexLeafIter::new(
-                        &self.hive,
+                    iter_try!(IndexLeafElement::elements_range(
                         count,
                         count_field_offset,
-                        subkeys_list.data_range
-                    ));
-                    Some(IndexRootInnerIter::IndexLeaf(iter))
+                        data_range
+                    ))
                 }
                 _ => unreachable!(),
             };
+
+            self.inner_info = Some(IndexRootIterInnerInfo {
+                signature,
+                elements_range,
+            });
         }
     }
 }
 
 impl<'a, B> FusedIterator for IndexRootIter<'a, B> where B: ByteSlice {}
 
-/// Inner iterator for a list of subkeys of an Index Root (common handling)
-/// Signature: li | lf | lh
-#[allow(clippy::enum_variant_names)]
-enum IndexRootInnerIter<'a, B: ByteSlice> {
-    FastLeaf(FastLeafIter<'a, B>),
-    HashLeaf(HashLeafIter<'a, B>),
-    IndexLeaf(IndexLeafIter<'a, B>),
+/// Inner "iterator" for Index Root to iterate over the linked Fast Leafs/Hash Leafs/Index Leafs.
+///
+/// We only use element indexes here and don't encapsulate any of the iterator structs (`FastLeafIter`/`HashLeafIter`/...)
+/// This wouldn't work for `IndexRootIterMut`, because iterator structs contain a reference to `Hive`
+/// and `IndexRootIterMut` already contains a mutable reference to `Hive`.
+struct IndexRootIterInnerInfo {
+    signature: [u8; 2],
+    elements_range: Range<usize>,
 }
 
-impl<'a, B> Iterator for IndexRootInnerIter<'a, B>
-where
-    B: ByteSlice,
-{
-    type Item = Result<KeyNode<&'a Hive<B>, B>>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self {
-            Self::FastLeaf(iter) => iter.next(),
-            Self::HashLeaf(iter) => iter.next(),
-            Self::IndexLeaf(iter) => iter.next(),
+impl IndexRootIterInnerInfo {
+    fn next_key_node_offset<B>(&mut self, hive: &Hive<B>) -> Option<u32>
+    where
+        B: ByteSlice,
+    {
+        match &self.signature {
+            b"lf" => FastLeafElement::next_key_node_offset(hive, &mut self.elements_range),
+            b"lh" => HashLeafElement::next_key_node_offset(hive, &mut self.elements_range),
+            b"li" => IndexLeafElement::next_key_node_offset(hive, &mut self.elements_range),
+            _ => unreachable!(),
         }
     }
 }
@@ -183,7 +186,7 @@ where
 pub(crate) struct IndexRootIterMut<'a, B: ByteSliceMut> {
     hive: &'a mut Hive<B>,
     elements_range: Range<usize>,
-    inner_info: Option<IndexRootIterMutInnerInfo>,
+    inner_info: Option<IndexRootIterInnerInfo>,
 }
 
 impl<'a, B> IndexRootIterMut<'a, B>
@@ -206,27 +209,15 @@ where
         })
     }
 
-    fn next_inner_key_node_offset(&mut self) -> Option<u32> {
-        // Retrieve the next element from the inner iterator.
-        let info = self.inner_info.as_mut()?;
-
-        match &info.signature {
-            b"lf" => FastLeafElement::next_key_node_offset(&self.hive, &mut info.elements_range),
-            b"lh" => HashLeafElement::next_key_node_offset(&self.hive, &mut info.elements_range),
-            b"li" => IndexLeafElement::next_key_node_offset(&self.hive, &mut info.elements_range),
-            _ => unreachable!(),
-        }
-    }
-
     pub(crate) fn next<'e>(&'e mut self) -> Option<Result<KeyNode<&'e mut Hive<B>, B>>> {
-        // Due to lifetimes and the borrow checker, the implementation of IndexRootIterMut
-        // is fundamentally different to IndexRootIter.
-        // This may be revisited when GATs have landed.
         loop {
-            if let Some(key_node_offset) = self.next_inner_key_node_offset() {
-                let cell_range = iter_try!(self.hive.cell_range_from_data_offset(key_node_offset));
-                let key_node = iter_try!(KeyNode::new(&mut *self.hive, cell_range));
-                return Some(Ok(key_node));
+            if let Some(inner_info) = self.inner_info.as_mut() {
+                if let Some(key_node_offset) = inner_info.next_key_node_offset(&self.hive) {
+                    let cell_range =
+                        iter_try!(self.hive.cell_range_from_data_offset(key_node_offset));
+                    let key_node = iter_try!(KeyNode::new(&mut *self.hive, cell_range));
+                    return Some(Ok(key_node));
+                }
             }
 
             // No inner iterator or the last inner iterator has been fully iterated.
@@ -275,15 +266,10 @@ where
                 _ => unreachable!(),
             };
 
-            self.inner_info = Some(IndexRootIterMutInnerInfo {
+            self.inner_info = Some(IndexRootIterInnerInfo {
                 signature,
                 elements_range,
             });
         }
     }
-}
-
-struct IndexRootIterMutInnerInfo {
-    signature: [u8; 2],
-    elements_range: Range<usize>,
 }
